66bf075eeac8763f37a8c8bdd6a13d5560011406
brainox <macmartins081@gmail.com>
2021-08-05 20:24:57 +0100

    added permissions to camera

---
+++ CameraFilterRxSwift/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilterRxSwift
+//
+//  Created by Decagon on 03/08/2021.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
---
+++ CameraFilterRxSwift/PhotosCollectionViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilterRxSwift
+//
+//  Created by Decagon on 02/08/2021.
+//
+
+import UIKit
+import Photos
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    private var images = [PHAsset]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populatePhotos()
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {fatalError("photoCollectionViewCell not found")}
+        let asset = self.images[indexPath.count]
+        let manager = PHImageManager.default()
+        
+        manager.requestImage(for: asset, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFill, options: nil) { image, _ in
+            DispatchQueue.main.async {
+                cell.photoImageView.image = image
+            }
+        }
+        return cell
+    }
+    
+    func populatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            
+            if status == .authorized {
+                // access the photos from the photos library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                self?.images.reverse()
+                DispatchQueue.main.async {
+                    self?.collectionView.reloadData()
+                }
+            }
+        }
+    }
+}
